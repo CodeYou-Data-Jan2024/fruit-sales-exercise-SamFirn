cb7787875ff4671c971672c3e5a5afe7ea435e23
Sam Firn <samfirn@gmail.com>
2024-05-14 18:02:09 -0400

    sorry its late like always

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,6 @@
-# add your code here
+import pandas as pd
+
+
+fruit_sales_by_year = pd.DataFrame({'Apples': [35,41], 'Bananas': [21,34]}, index = ['2017 Sales', '2018 Sales'])
+fruit_sales_by_year.to_csv('fruit.csv')
+print(fruit_sales_by_year)
